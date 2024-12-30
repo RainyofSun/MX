9065eacc783e00fccc42fdd27b29ba7bbe81366d
RainyofSun <liurant30@163.com>
2024-12-30 19:41:20 +0800

    基础工程创建

---
+++ MXCash/MXCash/APPDelegate/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MXCash
+//
+//  Created by Yu Chen  on 2024/12/30.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MXCash/MXCash/APPDelegate/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  MXCash
+//
+//  Created by Yu Chen  on 2024/12/30.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
